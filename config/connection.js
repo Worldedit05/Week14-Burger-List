bc282e1d3322d23422009ce13230007de200b316
Aaron Lee <worldedit05@gmail.com>
2016-11-06 19:04:00 -0600

    Setting up files

---
+++ config/connection.js
@@ -0,0 +1,22 @@
+/*
+Exporting the connection to the database
+*/
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+      port: 3306,
+      host: 'localhost',
+      user: 'root',
+      password: '',
+      database: 'burgers_db'
+});
+
+connection.connect(function (err) {
+      if (err) {
+            console.log('Error connecting: ' + err.stack);
+            return;
+      }
+      console.log('Connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
---
+++ config/orm.js
@@ -0,0 +1,21 @@
+/*
+ORM that takes inputs and converts them to mysql queries
+*/
+
+var connection = require('../config/connection.js');
+
+var orm = {
+        selectAll: function () {
+
+        },
+
+        insertOne: function () {
+
+        },
+
+        updateOne: function () {
+          
+        }
+};
+
+module.exports = orm;
