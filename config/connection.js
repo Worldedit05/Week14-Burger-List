3b6b057d0c224584627d57ade2353ca2a619da75
Aaron Lee <worldedit05@gmail.com>
2016-11-12 12:28:32 -0600

    Created a connection to the JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -3,13 +3,19 @@ Exporting the connection to the database
 */
 
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-      port: 3306,
-      host: 'localhost',
-      user: 'root',
-      password: '',
-      database: 'burgers_db'
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'burgers_db'
+  });
+}
 
 connection.connect(function (err) {
       if (err) {
